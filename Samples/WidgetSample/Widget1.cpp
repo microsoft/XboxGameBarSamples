1134751251d694cd54320c00f1e6141222fcebb9
Ben McGregor <benmc@microsoft.com>
2022-06-23 13:32:12 -0700

    Update restricted sample to RC

--- Samples/WidgetSample/Widget1.cpp
+++ Samples/WidgetSample/Widget1.cpp
@@ -2,13 +2,8 @@
 #include "Widget1.h"
 #include "Widget1.g.cpp"
 
-#include "winrt\Windows.System.h"
-#include <shellapi.h>
-
 using namespace winrt;
 using namespace winrt::Windows::UI::Xaml;
-using namespace winrt::Windows::UI::Xaml::Navigation;
-using namespace Microsoft::Gaming::XboxGameBar;widget
 
 namespace winrt::WidgetSample::implementation
 {
