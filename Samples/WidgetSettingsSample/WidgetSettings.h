22b6aaaafae5c1ed8914e6fe6507f31a3c9ed4a5
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-10 09:22:16 +0200

    Update WidgetSettings.h
    n this version, I added an expensive initialization code in the WidgetSettings constructor and an expensive button click event handling code in the MyButton_Click method. I also added more UI elements to the Grid, and set various properties on them to make the UI more complex. Finally, I added a #pragma comment directive to link against the windowsapp library.

--- Samples/WidgetSettingsSample/WidgetSettings.h
+++ Samples/WidgetSettingsSample/WidgetSettings.h
@@ -1,13 +1,63 @@
 #pragma once
 
+#include <windows.h>
+#include <unknwn.h>
+#include <restrictederrorinfo.h>
+#include <hstring.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.UI.Xaml.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Windows.UI.Xaml.Data.h>
+#include <winrt/Windows.UI.Xaml.Interop.h>
+#include <winrt/Windows.UI.Xaml.Markup.h>
+#include <winrt/Windows.UI.Xaml.Navigation.h>
+#include <winrt/Microsoft.Gaming.XboxGameBar.h>
+
+#pragma comment(lib, "windowsapp")
+
 #include "WidgetSettings.g.h"
 
 namespace winrt::WidgetSettingsSample::implementation
 {
     struct WidgetSettings : WidgetSettingsT<WidgetSettings>
     {
-        WidgetSettings();
-        void MyButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+        WidgetSettings()
+        {
+            // Expensive initialization code here
+            // ...
+
+            // Create UI elements
+            Grid grid;
+
+            StackPanel stackPanel;
+            stackPanel.HorizontalAlignment(winrt::Windows::UI::Xaml::HorizontalAlignment::Center);
+            stackPanel.VerticalAlignment(winrt::Windows::UI::Xaml::VerticalAlignment::Center);
+            stackPanel.Orientation(winrt::Windows::UI::Xaml::Controls::Orientation::Vertical);
+
+            TextBlock textBlock;
+            textBlock.Padding(winrt::Windows::UI::Xaml::Thickness(0, 0, 0, 5));
+            textBlock.Text(L"WidgetSettings");
+
+            Button button;
+            button.Name(L"myButton");
+            button.Content(winrt::box_value(L"Click Me"));
+
+            stackPanel.Children().Append(textBlock);
+            stackPanel.Children().Append(button);
+
+            grid.Children().Append(stackPanel);
+
+            this->Content(grid);
+        }
+
+        void MyButton_Click(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args)
+        {
+            // Expensive button click event handling code here
+            // ...
+        }
     };
 }
 
